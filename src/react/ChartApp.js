53c36aa824b7475be8c78d392e23467f33bf926a
peter@lidee.be <peter@lidee.be>
2018-10-08 15:56:31 +0200

    Building api requests

--- src/react/ChartApp.js
+++ src/react/ChartApp.js
@@ -5,7 +5,7 @@ class ChartApp extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      view: 'all', // all - single
+      view: 'single', // all - single
       country: 'Belgium',
       year: 2018,
       isLoading: true,
@@ -19,7 +19,7 @@ class ChartApp extends React.Component{
       'Belgium', 'France', 'Luxembourg', 'Germany', 'The Netherlands', 'Spain', 'Italy', 'Greece',
       'Portugal', 'Austria', 'Denmark', 'Norway', 'Sweden', 'Finland', 'Ireland', 'Poland', 'Estonia',
       'Czech Republic', 'Latvia', 'Lithuania', 'Slovak Republic', 'United Kingdom', 'Croatia', 'Serbia',
-      'Bosnia and Herzegovina', 'Bulgaria', 'Czech Republic', 'Hungary', 'Slovenia', 'Switzerland'
+      'Bosnia and Herzegovina', 'Bulgaria', 'Czech Republic', 'Hungary', 'Slovenia', 'Switzerland2'
     ];
     this.countries2 = [
       'Belgium', 'France'
@@ -74,40 +74,82 @@ class ChartApp extends React.Component{
     }*/
 
 
-    /*var apiRequest1 = fetch('http://api.population.io/1.0/population/Belgium/2018-01-01/').then(response => response.json());
-    var apiRequest2 = fetch('http://api.population.io/1.0/population/France/2018-01-01/').then(response => response.json());
-    //console.log(apiRequest1, apiRequest2);
+    // 2 cases:
+    // view all
+    // all the countries -> the total population -> 2018
+    // http://api.population.io/1.0/population/Belgium/2018-01-01/
+    // view single
+    // one country -> 20 years
+    // http://api.population.io/1.0/population/2018/Belgium/
 
-    var combinedData = {"apiRequest1":{},"apiRequest2":{}};
-    Promise.all([apiRequest1, apiRequest2]).then( values => {
-        combinedData["apiRequest1"] = values[0];
-        combinedData["apiRequest2"] = values[1];
-        //return combinedData;
-        console.log(combinedData);
-    });*/
+    if(this.state.view === 'all'){
 
-    const countriesRequests = this.countries.map(country => {
-      return fetch(`http://api.population.io/1.0/population/${country}/2018-01-01/`).then(response => response.json());
-    });
-    console.log('cr', countriesRequests);
+      const countriesRequests = this.countries.map(country => {
+        return fetch(`http://api.population.io/1.0/population/${country}/2018-01-01/`)
+          .then(response => {
+            // return false as value if the response was not ok
+            return response.ok ? response.json() : false;
+          })
+      });
+      //console.log('cr', countriesRequests);
 
-    var apiRequest1 = fetch('http://api.population.io/1.0/population/Belgium/2018-01-01/').then(response => response.json());
-    var apiRequest2 = fetch('http://api.population.io/1.0/population/France/2018-01-01/').then(response => response.json());
-    //console.log(apiRequest1, apiRequest2);
+      Promise.all(countriesRequests)
+        .then( values => {
+          //console.log(values);
+          const combinedData = values.map((value, i) => {
+            //console.log(value);
+            if(!value){ // if there was no response, return undefined as data
+              return { countryName: this.countries[i], population: undefined };
+            }
+            return { countryName: this.countries[i], population: value.total_population.population };
+          });
+          this.setState({
+            data: combinedData
+          });
+      })
+      .catch(error => this.setState({ error: error, isLoading: false }));
 
-    //var combinedData = {"apiRequest1":{},"apiRequest2":{}};
-    //const combinedData
-    Promise.all(countriesRequests).then( values => {
-        //console.log(values);
-        const combinedData = values.map((value, i) => {
-          console.log(value);
-          return { countryName: this.countries[i], population: value.total_population.population };
-        });
-        //combinedData["apiRequest1"] = values[0];
-        //combinedData["apiRequest2"] = values[1];
-        //return combinedData;
-        console.log(combinedData);
-    });
+    }
+
+
+
+    if(this.state.view === 'single'){
+
+      // make an array for all the years between 2018 - 20 years
+      const years = [];
+      for(let i = 0; i < 20; i++){
+        years.push(2018 - i);
+      }
+      years.push(1860);
+
+      const yearsRequests = years.map(year => {
+        return fetch(`http://api.population.io/1.0/population/${year}/Belgium/`)
+          .then(response => {
+            // return false as value if the response was not ok
+            return response.ok ? response.json() : false;
+          })
+      });
+      //console.log('cr', yearsRequests);
+
+      Promise.all(yearsRequests)
+        .then( values => {
+          //console.log(values);
+          const combinedData = values.map((value, i) => {
+            console.log(value);
+            if(!value){ // if there was no response, return undefined as data
+              return {'year': years[i], 'population': undefined};
+            }
+            return {'year': years[i], 'population': value};
+          });
+          this.setState({
+            data: combinedData
+          });
+      })
+      .catch(error => this.setState({ error: error, isLoading: false }));
+
+    }
+
+
 
     /*fetch(`http://api.population.io/1.0/population/${this.state.year}/${this.state.country}/`)
       .then(response => {
