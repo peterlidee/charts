d5ac8c2b8f0c04588c58471eba7407eb95c256af
peter@lidee.be <peter@lidee.be>
2018-10-10 18:21:25 +0200

    Managed to make click on tick work

--- src/react/Test.js
+++ src/react/Test.js
@@ -2,7 +2,103 @@ import React from 'react';
 import {HorizontalBar} from 'react-chartjs-2';
 import {prettyfyPopulationNum} from '../helpers.js'
 
+import { defaults } from 'react-chartjs-2';
+import { Chart } from 'react-chartjs-2';
+
+// Disable animating charts by default.
+//defaults.global.animation = false;
+
+console.log(defaults);
+
+
+
+
+
+
+/*defaults.global.plugin.register.onClick = function(e) {
+  console.log(e);
+  //update_caption(legendItem);
+  //original.call(this, e, legendItem);
+};*/
+
+/*defaults.scale.ticks.callback = {
+
+}*/
+
+//defaults.scale.ticks.display = false;
+
+/*defaults.global.plugin.register({
+  onEvent: function(chartInstance, chartEvent) {
+    var xAxis = chartInstance.scales['x-axis-0'];
+
+    // If mouse is over the legend, change cursor style to pointer, else don't show it
+    var x = chartEvent.x;
+    var y = chartEvent.y;
+
+    if (chartEvent.type === 'click' &&
+    	x <= xAxis.right && x >= xAxis.left &&
+      y <= xAxis.bottom && y >= xAxis.top) {
+      // category scale returns index here for some reason
+      var index = xAxis.getValueForPixel(x);
+      //document.getElementById('tick').innerHTML = chartInstance.data.labels[index];
+      console.log('click value?', chartInstance.data.labels[index]);
+    }
+  }
+});*/
+
+/*Chart.plugins.register({
+  onEvent: function(chartInstance, chartEvent) {
+    var xAxis = chartInstance.scales['x-axis-0'];
+
+    // If mouse is over the legend, change cursor style to pointer, else don't show it
+    var x = chartEvent.x;
+    var y = chartEvent.y;
+
+    if (chartEvent.type === 'click' &&
+    	x <= xAxis.right && x >= xAxis.left &&
+      y <= xAxis.bottom && y >= xAxis.top) {
+      // category scale returns index here for some reason
+      var index = xAxis.getValueForPixel(x);
+      document.getElementById('tick').innerHTML = chartInstance.data.labels[index];
+    }
+  }
+});*/
+
+
 class Test extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  test(tick){
+    console.log(handleTickClick());
+    //console.log('click', params[0], params[1])
+    //console.log(chart)
+  }
+
+  componentWillMount() {
+
+    //console.log(Chart.pluginService);
+    // chart.js does not have an onClick handler for ticks, so we write it here
+  	Chart.pluginService.register({
+      afterEvent: function handleTickClick(chartInstance, chartEvent) {
+        if( chartEvent.type === 'click'){
+          const yAxis = chartInstance.chart.chart.scales['y-axis-0'];
+          const x = chartEvent.x;
+          const y = chartEvent.y;
+          // only accept clicks in the area of the ticks (horizontal or vertical axis)
+          if ( x < yAxis.right && x > yAxis.left && y < yAxis.bottom && y > yAxis.top) {
+            // category scale returns index here for some reason
+            var index = yAxis.getValueForPixel(y);
+            //document.getElementById('tick').innerHTML = chartInstance.data.labels[index];
+            console.log('klik', chartInstance.data.labels[index]);
+            //this.test(chartInstance.data.labels[index]);
+            return chartInstance.data.labels[index];
+          }
+        }
+      }
+  	});
+  }
+
   render() {
     //console.log(this.props.data);
     const rawData = this.props.data.sort((a, b) => a.population < b.population);
@@ -41,6 +137,10 @@ class Test extends React.Component {
       },*/
       scales: {
         xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'population'
+          },
           ticks: {
             //minRotation: 90,
             //display: false,
@@ -48,15 +148,23 @@ class Test extends React.Component {
             //padding: 50
             // change the tick text
             callback: function(value, index, values) {
+              //console.log(value, index, values);
               return prettyfyPopulationNum(+value);
             }
-          }
+          },
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'countries'
+          },
         }]
       },
       tooltips: {
         // make tooltip visible when hover anywhere
         intersect: false,
         callbacks: {
+          // alter the labels to make numbers readable
           label: function(tooltipItem, data) {
             //console.log(tooltipItem, data);
 
@@ -68,8 +176,16 @@ class Test extends React.Component {
         }
       },
       legend: {
-        display: false
-      }
+        display: true
+      },
+      onClick: function(e, arr) {
+        //console.log(e.target, arr); //arr[0]._view.label
+      },
+      //{
+      	//onElementsClick: (elems) => { console.log(elems) },
+      	//getElementsAtEvent: (elems) => { console.log(elems) },
+      	// `elems` is an array of chartElements
+      //}
 
     }
     return (
