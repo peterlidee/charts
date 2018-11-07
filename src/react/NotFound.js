4820264d7940254131155d2befe7c0c434b876a8
peter@lidee.be <peter@lidee.be>
2018-11-07 18:14:29 +0100

    cleanup, remove console.log

--- src/react/NotFound.js
+++ src/react/NotFound.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = (props)=> {
-  console.log(props)
   return(
     <>
       <div className="chart__container">
