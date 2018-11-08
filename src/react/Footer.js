edcef9649bb4284429bb257cc6e18c0429de8fae
peter@lidee.be <peter@lidee.be>
2018-11-08 18:19:08 +0100

    added target blank to footer links

--- src/react/Footer.js
+++ src/react/Footer.js
@@ -5,22 +5,25 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__title">Population statistics of different European countries - A portfolio project -</div>
       <ul className="footer__meta">
-        <li><span className="footer__label">data api</span> <a href="http://api.population.io/">http://api.population.io</a></li>
+        <li>
+          <span className="footer__label">data api</span>
+          <a href="http://api.population.io/" target="_blank">http://api.population.io</a>
+        </li>
         <li><span className="footer__label">frameworks</span>
-          <a href="https://reactjs.org/">react.js</a>
+          <a href="https://reactjs.org/" target="_blank">react.js</a>
           |
-          <a href="https://www.npmjs.com/package/react-router-dom">react-router-dom</a>
+          <a href="https://www.npmjs.com/package/react-router-dom" target="_blank">react-router-dom</a>
           |
-          <a href="http://www.chartjs.org/">charts.js</a>
+          <a href="http://www.chartjs.org/" target="_blank">charts.js</a>
           /
-          <a href="https://www.npmjs.com/package/react-chartjs-2">react-chartjs-2</a>
+          <a href="https://www.npmjs.com/package/react-chartjs-2" target="_blank">react-chartjs-2</a>
         </li>
         <li><span className="footer__label">tools</span>
-          <a href="https://webpack.js.org/">webpack</a>
+          <a href="https://webpack.js.org/" target="_blank">webpack</a>
           |
-          <a href="https://sass-lang.com/">sass</a>
+          <a href="https://sass-lang.com/" target="_blank">sass</a>
         </li>
-        <li><span className="footer__label">sourcecode</span> <a href="https://github.com/peterlidee/charts">gitHub</a></li>
+        <li><span className="footer__label">sourcecode</span> <a href="https://github.com/peterlidee/charts" target="_blank">gitHub</a></li>
         <li><span className="footer__label">contact</span> <a href="mailto:peter@lidee.be">peter@lidee.be</a></li>
       </ul>
 
